2b2c6db658d7bea6a99e4abb93baa415e4265841
Andrew Angelle <andrew.angelle@rackspace.com>
2019-01-05 05:13:00 -0600

    folder updates

--- server/src/resolvers/types/Context.ts
+++
@@ -1,8 +0,0 @@
-import {Prisma}  from '../../generated/prisma-client'
-
-export interface Context {
-  db: Prisma;
-  request: any
-  Authorization: any
-  stripe: any;
-}
--- server/src/utils/keepWebAlive.ts
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch'
-
-//arbitrary task that runs every 10 minutes on heroku scheduler to keep dyno live
-(async function task() {
-  const results = await fetch('http://begincoffeesweb.herokuapp.com/');
-  return await results
-})();
