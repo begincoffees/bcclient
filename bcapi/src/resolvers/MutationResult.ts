552183e5ffb1cdd745cb8bcd83a8b776b0949132
Andrew Angelle <andrew.angelle@rackspace.com>
2019-01-05 01:18:35 -0600

    remove server folder - make own repo

--- bcapi/src/resolvers/MutationResult.ts
+++
@@ -1,9 +0,0 @@
-import { MutationResultResolvers } from "../generated/graphqlgen";
-
-export interface MutationResultParent {
-  success?: boolean;
-}
-
-export const MutationResult: MutationResultResolvers.Type = {
-  success: parent => parent.success
-};
